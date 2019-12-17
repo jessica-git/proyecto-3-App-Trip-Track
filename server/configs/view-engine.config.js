940e05f5a25a83d44b85aa5c9dedfad848c11a2f
Jessica <54844929+jessica-git@users.noreply.github.com>
2019-12-17 11:23:11 +0100

    Deploying

--- server/configs/view-engine.config.js
+++ server/configs/view-engine.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const favicon = require('serve-favicon');
 const express = require('express');
 
 module.exports = app => {
@@ -13,6 +14,6 @@ module.exports = app => {
     app.set('views', path.join(__dirname, '..', 'views'));
     app.set('view engine', 'hbs');
     app.use(express.static(path.join(__dirname, '..', 'public')));
-    app.use(path.join(__dirname, '..', 'public', 'images', 'favicon.ico'));
+    app.use(favicon(path.join(__dirname, '..', 'public', 'images')))
 
 }
